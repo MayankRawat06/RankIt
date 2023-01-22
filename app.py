76ae91be1cebcccaefb0dd81baba4cdbc9cf12cd
Mayank Rawat <mayank.rawat6802@gmail.com>
2023-01-22 23:37:56 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request
+from send_mail import send_mail
+app = Flask(__name__)
+def topsis(dataFile, weights, impacts, resFile = 'result.csv') :
+  import pandas as pd
+  import numpy as np
+  import sys
+  w = list(int(i) for i in weights.split(','))
+  im = list(i for i in impacts.split(','))
+  if(len(w) != len(im)) :
+    print('Number of elements in Weights and Impacts should be same')
+    sys.exit(0)
+  try:
+    data = pd.read_csv(dataFile)
+  except FileNotFoundError:
+    print('File not Found')
+  else :
+    df = data.iloc[:, 1 :].values
+    m = len(data)
+    n = len(data.columns) - 1
+    if(n < 3):
+      print('Less than 3 Columns')
+      sys.exit(0)
+    rss = []
+    for j in range(0, n):
+      s = 0
+      for i in range(0, m):
+        s += np.square(df[i, j])
+      rss.append(np.sqrt(s))
+    for j in range(0, n):
+      df[:, j] /= rss[j]
+      df[:, j] *= w[j]
+    best = []
+    worst = []
+    for j in range(0, n):
+      if(im[j] == '+'): 
+        best.append(max(df[:, j]))
+        worst.append(min(df[:, j]))
+      elif(im[j] == '-'):
+        best.append(min(df[:, j]))
+        worst.append(max(df[:, j]))
+      else:
+        print('Signs in Impact can be either + or - only')
+        sys.exit(0)
+    ebest = []
+    eworst = []
+    for i in range(0, m):
+      ssdb = 0
+      ssdw = 0
+      for j in range(0, n):
+        ssdb += np.square(df[i, j] - best[j])
+        ssdw += np.square(df[i, j] - worst[j])
+      rssdb = np.sqrt(ssdb)
+      rssdw = np.sqrt(ssdw)
+      ebest.append(rssdb)
+      eworst.append(rssdw)
+    p = []
+    for i in range(0, m):
+      measure = eworst[i] / (eworst[i] + ebest[i])
+      p.append(measure * 100)
+    data['Topsis Score'] = p
+    data['Rank'] = data['Topsis Score'].rank(ascending = False)
+    # print(data)
+    # data.to_csv(resFile)
+    return(data)
+@app.route("/", methods = ('GET', 'POST'))
+def hello_world():
+    if request.method  == "POST":
+        file = request.files['file']
+        weights = request.form['weights']
+        impacts = request.form['impacts']
+        email_id = request.form['email_id']
+        file_name = 'data.csv'
+        file.save(file_name)
+        data = topsis(file_name, weights, impacts)
+        send_mail(email_id, data.to_html())
+        return data.to_html()
+    return render_template('index.html')
